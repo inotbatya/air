ccf952969859215f3aff1d1a6043fee2c0158682
inotbatya <93325881+inotbatya@users.noreply.github.com>
2024-12-01 01:31:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import requests
 
+API_KEY = "93be52922464c8b6d8dc69c14553ab05"
+city = "Barnaul"
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    print("Город:", data["name"])
+    print("Температура:", data["main"]["temp"], "°C")
+    print("Качество воздуха:", data.get("aqi", "нет данных"))
+else:
+    print("Ошибка:", response.status_code)
