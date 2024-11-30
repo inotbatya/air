416f87f0458e76e40cc9d86cd36072dfd8b7477e
inotbatya <93325881+inotbatya@users.noreply.github.com>
2024-12-01 01:37:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import requests
 
-API_KEY = "93be52922464c8b6d8dc69c14553ab05"
-city = "Barnaul"
-url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+API_KEY = "ваш_API_ключ"
+lat = 53.3498  # Широта для Barnaul
+lon = 83.7637  # Долгота для Barnaul
+url_air = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API_KEY}"
 
-response = requests.get(url)
+response = requests.get(url_air)
 if response.status_code == 200:
-    data = response.json()
-    print("Город:", data["name"])
-    print("Температура:", data["main"]["temp"], "°C")
-    print("Качество воздуха:", data.get("aqi", "нет данных"))
+    air_data = response.json()
+    print("AQI:", air_data["list"][0]["main"]["aqi"])
+    print("PM2.5:", air_data["list"][0]["components"]["pm2_5"])
+    print("PM10:", air_data["list"][0]["components"]["pm10"])
 else:
     print("Ошибка:", response.status_code)
